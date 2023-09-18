1d114071df63c567e262d025521b7891508294ce
arshaan-abh <arshaan.git@gmail.com>
2023-09-18 12:01:50 +0330

    after review commit

--- src/components/tile/tile.tsx
+++ src/components/tile/tile.tsx
@@ -1,9 +1,10 @@
 import { MouseEventHandler } from 'react';
 
-export type TileType = {
+export interface TileType {
   number: number;
   onClick: MouseEventHandler<HTMLDivElement> | undefined;
-};
+}
+
 export default function Tile({ number, onClick }: TileType) {
   return (
     <div onClick={onClick} className="w-16 h-16 rounded bg-sky-500 flex items-center justify-center text-white">
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Tile from '@components/tile/tile.tsx';
 import EmptyTile from '@components/tile/empty-tile.tsx';
 import matrixCellSwap from '../../utils/matrix-cell-swap.ts';
@@ -6,22 +6,25 @@ import generateUniqueRandomMatrix from '../../utils/generate-random-matrix.ts';
 import { getLeftCell, getLowerCell, getRightCell, getUpperCell } from '../../utils/relative-matrix-cell.ts';
 
 export default function Home() {
-  const rows: number = 4;
-  const columns: number = 4;
-  const emptyCell: number = rows * columns;
+  const rows = 4;
+  const columns = rows;
+  const emptyCell = rows * columns;
 
   const [matrix, setMatrix] = useState<number[][] | null>(generateUniqueRandomMatrix(rows, columns));
 
-  function handleClick(row: number, col: number) {
-    if (getUpperCell(matrix, row, col) === emptyCell)
-      setMatrix((prevMatrix) => matrixCellSwap(prevMatrix, row, col, row - 1, col));
-    else if (getRightCell(matrix, row, col) === emptyCell)
-      setMatrix((prevMatrix) => matrixCellSwap(prevMatrix, row, col, row, col + 1));
-    else if (getLowerCell(matrix, row, col) === emptyCell)
-      setMatrix((prevMatrix) => matrixCellSwap(prevMatrix, row, col, row + 1, col));
-    else if (getLeftCell(matrix, row, col) === emptyCell)
-      setMatrix((prevMatrix) => matrixCellSwap(prevMatrix, row, col, row, col - 1));
-  }
+  const handleClick = useCallback(
+    (row: number, col: number) => {
+      if (getUpperCell(matrix, row, col) === emptyCell)
+        setMatrix((prevMatrix) => matrixCellSwap(prevMatrix, row, col, row - 1, col));
+      else if (getRightCell(matrix, row, col) === emptyCell)
+        setMatrix((prevMatrix) => matrixCellSwap(prevMatrix, row, col, row, col + 1));
+      else if (getLowerCell(matrix, row, col) === emptyCell)
+        setMatrix((prevMatrix) => matrixCellSwap(prevMatrix, row, col, row + 1, col));
+      else if (getLeftCell(matrix, row, col) === emptyCell)
+        setMatrix((prevMatrix) => matrixCellSwap(prevMatrix, row, col, row, col - 1));
+    },
+    [matrix, emptyCell],
+  );
 
   if (matrix === null) {
     return <div className="flex items-center justify-center text-red-500">There is a problem with the matrix.</div>;
@@ -32,9 +35,11 @@ export default function Home() {
       {matrix.map((row, rowIndex) =>
         row.map((cell, columnIndex) => {
           if (cell === emptyCell) return <EmptyTile key={emptyCell} />;
-          else return <Tile number={cell} onClick={() => handleClick(rowIndex, columnIndex)} key={cell}></Tile>;
+          return <Tile number={cell} onClick={() => handleClick(rowIndex, columnIndex)} key={cell}></Tile>;
         }),
       )}
     </div>
   );
 }
+
+// todo use framer motion for animation
