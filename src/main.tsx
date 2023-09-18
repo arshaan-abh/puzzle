efe0cb627f1a3976d20718748fb3a2163bba82f7
arshaan-abh <arshaan.git@gmail.com>
2023-09-18 11:44:27 +0330

    use strict mode again

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './app.tsx';
 import './styles/index.scss';
+import { StrictMode } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // I (Arshaan) had to remove the strictMode because it would call the set state methods twice. Which in the setMatrix case, it would sabotage its functionality (Since the second run would undo the first).
-  <App />
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
